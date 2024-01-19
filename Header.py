eeb4ce6ef06bebe6dfa86a8a548b43f333c5a660
Sanisetty-bhavya <114378144+Sanisetty-bhavya@users.noreply.github.com>
2024-01-19 16:14:24 +0530

    Create Header.py

---
+++ Header.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import tree
+from collections import defaultdict
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.tree import plot_tree
